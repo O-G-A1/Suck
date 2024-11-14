9a3fdf7edbc86a1c65908dfef18ba97bd5b1d818
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-11-14 14:12:53 +0100

    bar

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ setTimeout(()=>{
 
 
 },1500);
+
+function toggleMenu(){
+    const menu=document.getElementById("dropdown-menu");
+    const hamburger=document.querySelector(".bar_menu");
+
+    menu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+}
