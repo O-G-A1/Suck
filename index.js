ee618f0232ce95026d0384fa90a74bfb91d17165
Gbenga Ogunsola <gbengaadeboye99@gmail.com>
2024-11-28 12:51:56 +0100

    suck

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-setTimeout(()=>{
-    document.getElementById("loadscreen").style.display="none";
+setTimeout(() => {
+  document.getElementById("loadscreen").style.display = "none";
 
-    document.getElementById("maincontent").style.display="block";
+  document.getElementById("maincontent").style.display = "block";
+}, 300);
 
+function toggleMenu() {
+  const menu = document.getElementById("dropdown-menu");
+  const hamburger = document.querySelector(".bar_menu");
 
-},1500);
-
-function toggleMenu(){
-    const menu=document.getElementById("dropdown-menu");
-    const hamburger=document.querySelector(".bar_menu");
-
-    menu.classList.toggle("active");
-    hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#splide", {
+    type: "loop",
+    perPage: 1,
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    gap: "2rem",
+  }).mount();
+});
